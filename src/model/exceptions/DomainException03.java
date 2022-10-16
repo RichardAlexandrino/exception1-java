8555f05dd72d4d966aa3fa4749e70796650f3dbc
Richard Alexandrino de Macedo <richardalexandrino@hotmail.com>
2022-10-15 23:15:45 -0300

    Good solution

---
+++ src/model/exceptions/DomainException03.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class DomainException03 extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DomainException03(String msg) {
+		super(msg);
+	}
+	
+}
