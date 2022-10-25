8658388a1f3680f8a7cb8459c0156336465c8f02
Richard Alexandrino de Macedo <richardalexandrino@hotmail.com>
2022-10-25 18:14:14 -0300

    Add Program04 - Bank Withdraw

--- src/application/Program04.java
+++ src/application/Program04.java
@@ -1,10 +1,54 @@
+/* Write a program to read data from a bank account and then
+ * perform a withdraw from this bank account, showing the new
+ * balance. A withdraw cannot occur or if there is no balance
+ * in the account, or if the amount of the withdraw is greater
+ * than the limit of account withdraw. */
+
 package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account04;
+import model.exceptions.DomainException03;
 
 public class Program04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account04 account = new Account04(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.println(account);
+		}
+		catch (DomainException03 e){
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		sc.close();
+		
 	}
 
 }
--- src/model/entities/Account04.java
+++ src/model/entities/Account04.java
@@ -1,5 +1,67 @@
 package model.entities;
 
+import model.exceptions.DomainException03;
+
 public class Account04 {
 
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account04(Integer number, String holder, Double balance, Double withdrawLimit) {
+	
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		this.balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		if (amount > withdrawLimit) {
+			throw new DomainException03("The amount exceeds withdraw limit");
+		}
+		if (amount > balance) {
+			throw new DomainException03("Not enough balance");
+		}
+		this.balance -= amount;
+	}
+
+	@Override
+	public String toString() {
+		return "New balance: " + String.format("%.2f", balance);
+	}
+	
 }
